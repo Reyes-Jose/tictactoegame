57d74bb2dee18c6d2e3e1b5d553b3de8422516c4
JoeDoom <reyesj1209@gmail.com>
2023-03-24 08:18:41 -0700

    adds logic for game.

--- tictactoegame/src/TicTacToe.js
+++ tictactoegame/src/TicTacToe.js
@@ -1,35 +1,98 @@
-import React from 'react'
-import './tictactoestyle.css'
-
+import React, { useState } from "react";
+import "./tictactoestyle.css";
 
 export default function TicTacToe() {
-    const handleClick = () =>{
-        
+  const [turn, setTurn] = useState("x");
+  const [cells, setCells] = useState(Array(9).fill(""));
+  const [winner, setWinner] = useState(null);
+
+  const checkForWinner = (squares) => {
+    let combos = {
+      across: [
+        [0, 1, 3],
+        [3, 4, 5],
+        [6, 7, 8],
+      ],
+      down: [
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+      ],
+      diagnol: [
+        [0, 4, 8],
+        [2, 4, 6],
+      ],
+    };
+    for (let combo in combos) {
+      combos[combo].forEach((pattern) => {
+        if (
+          squares[pattern[0]] === "" ||
+          squares[pattern[1]] === "" ||
+          squares[pattern[2]] === ""
+        ) {
+        } else if (
+          squares[pattern[0]] === squares[pattern[1]] &&
+          squares[pattern[1]] === squares[pattern[2]]
+        ) {
+          setWinner(squares[pattern[0]]);
+        }
+      });
     }
-    const Cell = () => {
-        return <td onClick={() => handleClick()}>-</td>
+  };
+
+  const handleClick = (num) => {
+    if (cells[num] !== "") {
+      alert("Already Clicked!!");
+      return;
     }
+    let squares = [...cells];
+
+    if (turn === "x") {
+      squares[num] = "x";
+      setTurn("o");
+    } else {
+      squares[num] = "o";
+      setTurn("x");
+    }
+
+    checkForWinner(squares);
+    setCells(squares);
+  };
+  const handleRestart = () => {
+    setWinner(null);
+    setCells(Array(9).fill(""));
+  };
+  const Cell = ({ num }) => {
+    return <td onClick={() => handleClick(num)}>{cells[num]}</td>;
+  };
   return (
-    <div className='container'>
-        <table>
-            <tbody>
-                <tr>
-                    <Cell/>
-                    <Cell/>
-                    <Cell/>
-                </tr>
-                <tr>
-                    <Cell/>
-                    <Cell/>
-                    <Cell/>
-                </tr>
-                <tr>
-                    <Cell/>
-                    <Cell/>
-                    <Cell/>
-                </tr>
-            </tbody>
-        </table>
+    <div className="container">
+      <table>
+        <tbody>
+          Turn: {turn}
+          <tr>
+            <Cell num={0} />
+            <Cell num={1} />
+            <Cell num={2} />
+          </tr>
+          <tr>
+            <Cell num={3} />
+            <Cell num={4} />
+            <Cell num={5} />
+          </tr>
+          <tr>
+            <Cell num={6} />
+            <Cell num={7} />
+            <Cell num={8} />
+          </tr>
+        </tbody>
+      </table>
+      {winner && (
+        <div>
+          <p>{winner} wins!</p>
+          <button onClick={() => handleRestart()}>Play again</button>
+        </div>
+      )}
     </div>
-  )
+  );
 }
